a180c66f942fed0bd7f0a8b8f97fa034ed6a4de7
[dyad] <huulong111@gmail.com>
2025-11-12 01:30:54 +0700

    [dyad] Điều chỉnh bố cục toàn màn hình - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,10 +5,10 @@ import ContentPanel from '@/components/ContentPanel';
 
 const Index = () => {
   return (
-    <div className="bg-[#F6F8FA] min-h-screen">
-      <div className="w-[1440px] mx-auto">
+    <div className="bg-[#F6F8FA] min-h-screen flex flex-col">
+      <div className="w-[1440px] mx-auto flex flex-col flex-1">
         <Header />
-        <main className="flex items-start self-stretch my-3 mx-3 gap-3">
+        <main className="flex flex-1 items-stretch my-3 mx-3 gap-3">
           <Sidebar />
           <div className="flex items-start bg-white flex-1 rounded-lg border border-solid border-[#EDEDED]">
             <AIPanel />
